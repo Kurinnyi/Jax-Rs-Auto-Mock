59dde424ad4e78ad1f3bff9afda6f1579063e48a
Kurinnyi Ivan <kurinnyi.ivan@ukr.net>
2018-10-22 23:31:15 +0300

    Return JSONs on errors. Add logging

--- src/main/kotlin/ua/kurinnyi/jaxrs/auto/mock/StubNotFoundExceptionMapper.kt
+++ src/main/kotlin/ua/kurinnyi/jaxrs/auto/mock/StubNotFoundExceptionMapper.kt
@@ -1,16 +1,10 @@
 package ua.kurinnyi.jaxrs.auto.mock
 
-import javax.ws.rs.core.Response
 import javax.ws.rs.ext.ExceptionMapper
 import javax.ws.rs.ext.Provider
 
 
 @Provider
-class StubNotFoundExceptionMapper : ExceptionMapper<StubNotFoundException> {
-
-    override fun toResponse(exception: StubNotFoundException): Response {
-        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                .entity(exception.message)
-                .build()
-    }
+class StubNotFoundExceptionMapper : ErrorHandler<StubNotFoundException>, ExceptionMapper<StubNotFoundException> {
+    override fun getMessage(exception: StubNotFoundException) = exception.message
 }
