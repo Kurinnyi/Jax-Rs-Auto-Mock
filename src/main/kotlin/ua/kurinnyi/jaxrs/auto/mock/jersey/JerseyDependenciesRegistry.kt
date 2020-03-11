f1b2bfc8d9e056e3376ed2f7c6188de1eb800c89
Kurinnyi Ivan <kurinnyi.ivan@ukr.net>
2020-03-11 22:24:24 +0200

    Add docs to StubServer

--- src/main/kotlin/ua/kurinnyi/jaxrs/auto/mock/jersey/JerseyDependenciesRegistry.kt
+++ src/main/kotlin/ua/kurinnyi/jaxrs/auto/mock/jersey/JerseyDependenciesRegistry.kt
@@ -4,19 +4,15 @@ import ua.kurinnyi.jaxrs.auto.mock.DependenciesRegistry
 import ua.kurinnyi.jaxrs.auto.mock.GroupSwitchService
 import ua.kurinnyi.jaxrs.auto.mock.MethodInvocationHandler
 import ua.kurinnyi.jaxrs.auto.mock.ProxyInstanceFactory
-import ua.kurinnyi.jaxrs.auto.mock.extensions.ResponseBodyProvider
+import ua.kurinnyi.jaxrs.auto.mock.extensions.*
 import ua.kurinnyi.jaxrs.auto.mock.response.TemplateEngine
 import ua.kurinnyi.jaxrs.auto.mock.filters.HttpRequestResponseHolder
 import ua.kurinnyi.jaxrs.auto.mock.filters.ResponseIntersectingFilter
-import ua.kurinnyi.jaxrs.auto.mock.extensions.ProxyConfiguration
 import ua.kurinnyi.jaxrs.auto.mock.response.RequestProxy
 import ua.kurinnyi.jaxrs.auto.mock.mocks.SerialisationUtils
 import ua.kurinnyi.jaxrs.auto.mock.mocks.StubDefinitionsExecutor
 import ua.kurinnyi.jaxrs.auto.mock.mocks.StubsDefinition
-import ua.kurinnyi.jaxrs.auto.mock.extensions.RecordSaver
 import ua.kurinnyi.jaxrs.auto.mock.response.Recorder
-import ua.kurinnyi.jaxrs.auto.mock.extensions.HttpResponseDecoder
-import ua.kurinnyi.jaxrs.auto.mock.extensions.SerialisedMocksFilesLoader
 import ua.kurinnyi.jaxrs.auto.mock.extensions.defaul.*
 import ua.kurinnyi.jaxrs.auto.mock.serializable.*
 
@@ -39,8 +35,8 @@ object JerseyDependenciesRegistry : DependenciesRegistry {
     private val contextSaveFilter = HttpRequestResponseHolder()
     private val requestProxy = RequestProxy(contextSaveFilter)
     private val platformUtils = JerseyPlatformUtils()
-    private val yamlObjectMapper = YamlObjectMapper()
 
+    var serializableMocksObjectMapper: SerializableObjectMapper = YamlObjectMapper()
     var recordSaver: RecordSaver = ConsoleRecordSaver()
     var httpResponseDecoders: List<HttpResponseDecoder> = listOf()
     var proxyConfiguration: ProxyConfiguration = ForwardWhenNothingMatchedProxyConfiguration()
@@ -49,7 +45,7 @@ object JerseyDependenciesRegistry : DependenciesRegistry {
     var defaultResponseBodyProvider: ResponseBodyProvider = ResourceFolderFilesResponseBodyProvider(jacksonBodyProvider)
 
     private val recorder: Recorder by lazy {
-        Recorder(httpResponseDecoders, recordSaver, contextSaveFilter, responseIntersectingFilter, platformUtils, yamlObjectMapper)
+        Recorder(httpResponseDecoders, recordSaver, contextSaveFilter, responseIntersectingFilter, platformUtils, serializableMocksObjectMapper)
     }
     private val commonDependencies: CommonDependencies by lazy {
         CommonDependencies(proxyConfiguration, stubDefinitions, this)
@@ -58,7 +54,7 @@ object JerseyDependenciesRegistry : DependenciesRegistry {
         SerialisationUtils(defaultResponseBodyProvider, TemplateEngine())
     }
     val serializableMocksLoader: SerializableMocksLoader by lazy {
-        SerializableMocksLoader(serialisedMocksFilesLoader, yamlObjectMapper, SerializableMockToExecutableMethodMockConverter())
+        SerializableMocksLoader(serialisedMocksFilesLoader, serializableMocksObjectMapper, SerializableMockToExecutableMethodMockConverter())
     }
 
     data class CommonDependencies(val proxyConfiguration: ProxyConfiguration,
