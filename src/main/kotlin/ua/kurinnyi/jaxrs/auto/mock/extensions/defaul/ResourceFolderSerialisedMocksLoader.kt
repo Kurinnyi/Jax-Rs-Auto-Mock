3de2d6d387d424eff111996db018c15ba7544dde
Kurinnyi Ivan <kurinnyi.ivan@ukr.net>
2024-04-28 22:24:44 -0700

    Add publishing to github maven repository

--- src/main/kotlin/ua/kurinnyi/jaxrs/auto/mock/extensions/defaul/ResourceFolderSerialisedMocksLoader.kt
+++ src/main/kotlin/ua/kurinnyi/jaxrs/auto/mock/extensions/defaul/ResourceFolderSerialisedMocksLoader.kt
@@ -10,7 +10,6 @@ import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
 import java.util.stream.Stream
-import kotlin.streams.toList
 
 /**
  * This class is used to load serializable mocks.
